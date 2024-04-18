83af20956140354d8d79e7bcb0252ccb44d58e80
ejones22 <143116175+ejones22@users.noreply.github.com>
2024-04-18 04:16:55 +0000

    final

--- answers.md
+++ answers.md
@@ -1,6 +1,6 @@
 # CMPS 2200 Recitation 10## Answers
 
-**Name:**_________________________
+**Name:**___Emma Jones______
 **Name:**_________________________
 
 
@@ -9,9 +9,10 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 
 - **2)**
-
+W = O(n+m)
 - **4)**
-
+O(n), when reachable is called and each node is traversed
 - **5)**
-
+W = O(n+M)
 - **7)**
+ If we switched the graph representation to an adjacency matrix, the work of reachable changes to O(n) per node. This is because in adjacency matrices, each connection must be evaluated in its own operation.
--- main.py
+++ main.py
@@ -10,33 +10,41 @@ def make_undirected_graph(edge_list):
 
 
 def reachable(graph, start_node):
-    """
-    Returns:
-      the set of nodes reachable from start_node
-    """
-    result = set([start_node])
-    frontier = set([start_node])
-    while len(frontier) != 0:
-        ###TODO
-        pass
-    return result
-
-
-
+  """
+  Returns:
+  the set of nodes reachable from start_node
+  """
+  result = set([start_node])
+  frontier = set([start_node])
+  while len(frontier) != 0:
+      current_node = frontier.pop()
+      neighbors = graph[current_node]
+      for n in neighbors:
+          if n not in result:
+              result.add(n)
+              frontier.add(n)
+      
+  return result
 
 
 def connected(graph):
-    ### TODO
-    pass
-
-
+  start = next(iter(graph))
+  reached = reachable(graph, start)
+  return len(reached) == len(graph) 
 
 
 def n_components(graph):
-    """
-    Returns:
-      the number of connected components in an undirected graph
-    """
-    ### TODO
-    pass
+  """
+  Returns:
+  the number of connected components in an undirected graph
+  """
+  visited = set()
+  inc = 0
+  for node in graph:
+    if (node in visited) is False:
+        inc += 1  
+        component_nodes = reachable(graph, node)
+        visited.update(component_nodes)
+  return inc
+
 
